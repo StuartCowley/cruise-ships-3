7f33f3c303571524eb1d25d04979febd5c81a783
Chris Peakin <christopher.peakin@googlemail.com>
2022-01-16 15:00:45 +0000

    updated itinerary test

--- __tests__/Itinerary.test.js
+++ __tests__/Itinerary.test.js
@@ -1,14 +1,32 @@
 const Itinerary =  require('../src/Itinerary.js');
-const Port = require('../src/Port.js');
 
 describe('Itinerary', () =>{
-    it('it can be instantiated', () =>{
-        expect(new Itinerary()).toBeInstanceOf(Object);
-    });
-    it('can have ports', () =>{
-        const dover = new Port('Dover');
-        const calais = new Port('Calais');
-        const itinerary = new Itinerary([dover, calais]);
-        expect(itinerary.ports).toEqual([dover, calais]);
-    });
+    describe('with iterineraries', () =>{
+        let dover;
+        let calais;
+        let itinerary;
+        beforeEach(() => {
+            dover = {
+                addShip: jest.fn(),
+                removeShip: jest.fn(),
+                portName: 'Dover',
+                ships: []
+            };
+            calais = {
+                addShip: jest.fn(),
+                removeShip: jest.fn(),
+                portName: 'Calais',
+                ships: []
+            };
+            itinerary = {
+                ports: [dover, calais]
+            };
+        });
+        it('it can be instantiated', () =>{
+            expect(new Itinerary()).toBeInstanceOf(Object);
+        });
+        it('can have ports', () =>{
+            expect(itinerary.ports).toEqual([dover, calais]);
+        });
+    });    
 });
